490ce5a28e1ff7be373aa6f91ac4e8dc7b293bc8
DennisDaddy <softteez95@gmail.com>
2016-10-15 10:34:41 +0300

    add message params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,19 @@ class MessagesController < ApplicationController
 	end
 
 	def new
-		
+		@message = Message.new
 	end
 
 	def create
+		@message = Message.new(message_params)
+		
+	end
+
+	private
+
+	def message_params
+
+		params.require(:message).permit(:title, :description)
 		
 	end
 end
