b207aab96586e8d1ba87b4f36cb8a8c400dc48c5
DennisDaddy <softteez95@gmail.com>
2016-10-15 11:10:05 +0300

    enable message save and redirect

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,12 @@ class MessagesController < ApplicationController
 
 	def create
 		@message = Message.new(message_params)
+		if @message.save
+			redirect_to root_path
+		else
+			render 'new'
+			
+		end
 		
 	end
 
